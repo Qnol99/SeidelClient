b3b596df4c7bedd791959064693dc85ab0f44602
vltrxyz <vltr@vltr.xyz>
2026-07-02 13:18:29 +0200

    Improved the command window and message scrolling

--- minecraft/src/net/minecraft/src/GuiChat.java
+++ minecraft/src/net/minecraft/src/GuiChat.java
@@ -1,11 +1,15 @@
 package net.minecraft.src;
 
 import org.lwjgl.input.Keyboard;
+import java.util.LinkedList;
 
 public class GuiChat extends GuiScreen {
 	protected String message = "";
 	private int updateCounter = 0;
 	private static final String field_20082_i = ChatAllowedCharacters.allowedCharacters;
+	protected static LinkedList<String> commandHistory = new LinkedList<String>();
+	protected int commandIndex = -1;
+	protected String lastMessage = "";
 
 	public void initGui() {
 		Keyboard.enableRepeatEvents(true);
@@ -20,19 +24,45 @@ public class GuiChat extends GuiScreen {
 	}
 
 	protected void keyTyped(char var1, int var2) {
-		if(var2 == 1) {
+		if(var2 == 1) { // ESC
 			this.mc.displayGuiScreen((GuiScreen)null);
-		} else if(var2 == 28) {
+		} else if(var2 == 28) { // RETURN
 			String var3 = this.message.trim();
 			if(var3.length() > 0) {
 				String var4 = this.message.trim();
 				if(!this.mc.lineIsCommand(var4)) {
 					this.mc.thePlayer.sendChatMessage(var4);
 				}
+				commandHistory.add(var4);
 			}
 
 			this.mc.displayGuiScreen((GuiScreen)null);
-		} else {
+		} else if(var2 == 200 && commandHistory.size() != 0) { // UP
+			if(this.commandIndex == -1) {
+				this.commandIndex = commandHistory.size();
+				this.lastMessage = this.message;
+			}
+
+			if(this.commandIndex == 0)
+				return;
+
+			this.commandIndex--;
+			this.message = commandHistory.get(commandIndex);
+
+		} else if(var2 == 208) { // DOWN
+			if(this.commandIndex == -1)
+				return;
+				
+			if(this.commandIndex == commandHistory.size() -1) {
+				this.commandIndex = -1;
+				this.message = lastMessage;
+			} else {
+				this.commandIndex++;
+				this.message = commandHistory.get(commandIndex);
+			}
+				
+
+		} else { // any character
 			if(var2 == 14 && this.message.length() > 0) {
 				this.message = this.message.substring(0, this.message.length() - 1);
 			}
